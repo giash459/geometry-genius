d2f3a3294e8d6dd9e9671e2bcaf76ebedbd2dd71
Md Giash <mdgiash999ctg@gmail.com>
2023-08-18 08:36:25 +0600

    finished the rectangle card section

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -10,11 +10,35 @@ function calculateTriangleArea(){
     const heightValueText  = heightField.value;
     const height = parseFloat(heightValueText);
     console.log(height);
+
+    // calculate triangle area
     const area = 0.5 * base * height;
-    console.log(area);
+    // console.log(area);
 
     // show triangle area
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
+
+}
+function calculateRectangleArea(){
+    // get rectangle width value
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    // get length width value
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    // calculate Rectangle area
+    const area = width * length;
+    // console.log(area);
+
+    //  show Rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 
 }
